07fdd9f360af2ad4f6ec258105a5b328e5fe0d60
victorfeagins <victorfeagins@att.net>
2021-06-14 10:49:40 -0400

    Started work on extracting variables from .nc files

---
+++ Extracting_NetCDF_Variables.R
@@ -0,0 +1,10 @@
+# Transforming NetCdf files to Raster -----------
+# Author: Victor Feagins
+# Description: Turning .nc files to raster format that is easier to work with.
+
+
+## Packages ----
+library(ncdf4)
+library(ncdf4.helpers)
+library(raster)
+
