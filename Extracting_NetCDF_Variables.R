8d83994192052c84a72588aba62c110456af3f26
victorfeagins <victorfeagins@att.net>
2021-06-14 14:44:44 -0400

    Offset and scalefactor extracted successfully. Need to make it general

--- Extracting_NetCDF_Variables.R
+++ Extracting_NetCDF_Variables.R
@@ -1,10 +1,71 @@
-# Transforming NetCdf files to Raster -----------
+# Extracting Variables From NetCDF -----------
 # Author: Victor Feagins
-# Description: Turning .nc files to raster format that is easier to work with.
+# Description: Extracting from .nc files variables I need to do analysis
 
 
 ## Packages ----
 library(ncdf4)
 library(ncdf4.helpers)
-library(raster)
+library(stringr)
 
+## File ----
+
+filename = "Data/OR_ABI-L1b-RadC-M3C02_G16_s20172330202189_e20172330204562_c20172330205000.nc"
+#filename = "Data/OR_ABI-L1b-RadC-M3C02_G16_s20172330207189_e20172330209562_c20172330210002.nc"
+#filename = "Data/OR_ABI-L1b-RadC-M3C03_G16_s20172330312189_e20172330314562_c20172330315004.nc"
+#filename = "Data/OR_ABI-L2-ACMC-M4_G16_s20172330110227_e20172330110227_c20172330116257.nc"
+NC_file <- nc_open(filename)
+
+## Variables Extraction ----
+
+### Coordinate ----
+
+# Offset and factors found in print(NC_Test) having hard time extracting it out of the variable.
+print.output<- capture.output(NC_file)
+
+y.scale_factor = print.output[grep("scale_factor",print.output)][2] %>% 
+  str_extract("-?[\\d]+.?[\\d]+e?-?[\\d]+") %>% 
+  as.numeric()
+
+x.scale_factor = print.output[grep("scale_factor",print.output)][3] %>% 
+  str_extract("-?[\\d]+.?[\\d]+e?-?[\\d]+") %>% 
+  as.numeric()
+
+
+y.offset = print.output[grep("add_offset",print.output)][2] %>% 
+  str_extract("-?[\\d]+.?[\\d]+e?-?[\\d]+") %>% 
+  as.numeric()
+
+x.offset = print.output[grep("add_offset",print.output)][3] %>% 
+  str_extract("-?[\\d]+.?[\\d]+e?-?[\\d]+") %>% 
+  as.numeric()
+
+
+nc_close(NC_file)
+### Radiances ----
+
+variable = "Rad"
+attributes(NC_file$var$Rad)
+
+# See section 5.1 on Volume 1 GOES R SERIES PRODUCT DEFINITION AND USERS’ GUIDE
+Scale_Factor <- NC_file$var[[variable]]$scaleFact
+Offset <- NC_file$var[[variable]]$addOffset
+Missing_val <- NC_file$var[[variable]]$missval
+
+Kappa = ncvar_get(NC_file,"kappa0")
+
+
+### Data Quality Flags ----
+#See Section 5.1.3.6.4 on Volume 3 Level 1b Products on Data Quality Flag Values
+attributes(NC_file$var$DQF)
+
+DataQualityFlag = ncvar_get(NC_file,"DQF")
+
+
+### Time ----
+# See section 5.0.1 of Volume 3 Level 1b products
+# Time recorded as seconds from 2000-01-01 12:00:00 UTC
+attributes(NC_file$var$time_bounds)
+Time_Bounds <-  ncvar_get(NC_file,"time_bounds")
+as.POSIXct(test[1], origin = "2000-01-01 12:00:00", tz = "UTC")
+as.POSIXct(test[2], origin = "2000-01-01 12:00:00", tz = "UTC")
