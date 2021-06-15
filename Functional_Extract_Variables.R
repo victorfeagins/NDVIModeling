6503ae855f3879b6c8dddd015336a6b8094f221f
victorfeagins <victorfeagins@att.net>
2021-06-15 13:52:38 -0400

    Coords.to.YX work. Coords.to.index don't work.

---
+++ Functional_Extract_Variables.R
@@ -0,0 +1,172 @@
+# Functional Extracting Variables From NetCDF by coordinates -----------
+# Author: Victor Feagins
+# Description: Extracting from .nc files variables I need to do analysis in a functional manner
+
+## Packages ----
+library(ncdf4)
+library(ncdf4.helpers)
+library(stringr)
+library(magrittr)
+## Utility functions ----
+rad2deg <- function(rad) {(rad * 180) / (pi)}
+deg2rad <- function(deg) {(deg * pi) / (180)}
+
+sin.sq <- function(num){
+  (sin(num))^2
+}
+cos.sq <- function(num){
+  (cos(num))^2
+}
+coords.to.YX <- function(lat, long, NC_infolist){
+  #Lat and long in GRS80
+  attach(NC_infolist)
+  phi.c = lat %>%
+    tan() %>% 
+    multiply_by((r.pol/r.eq)^2) %>% 
+    atan()
+  
+  r.c = phi.c %>% 
+    cos.sq() %>% 
+    multiply_by(e.value^2) %>% 
+    multiply_by(-1) %>% 
+    add(1) %>% 
+    raise_to_power(-1/2) %>% 
+    multiply_by(r.pol)
+  
+  s.x = long %>% 
+    subtract(lambda.o) %>% 
+    cos() %>% 
+    multiply_by(cos(phi.c)) %>% 
+    multiply_by(-r.c) %>% 
+    add(H)
+  
+  s.y = long %>% 
+    subtract(lambda.o) %>% 
+    sin() %>% 
+    multiply_by(cos(phi.c)) %>% 
+    multiply_by(-r.c)
+  
+  s.z = phi.c %>% 
+    sin() %>% 
+    multiply_by(r.c)
+  
+  y = atan(s.z/s.x)
+  
+  x = s.x^2 %>% 
+    add(s.y^2) %>% 
+    add(s.z^2) %>% 
+    raise_to_power(-1/2) %>% 
+    multiply_by(-s.y) %>% 
+    asin()
+  
+  list(y.rad = y, x.rad = x)
+}
+## File Info ----
+
+
+
+File_info <- function(NC_file){
+  number_pattern <- "-?[\\d]+.?[\\d]*e?-?[\\d]+"
+  
+  ### Printing_output ----
+  print.output<- capture.output(NC_file)
+  
+  ### Coordinate Information -----
+  Factor_Offset<- print.output %>% 
+    str_subset("scale_factor|add_offset") #looking for scale factor or add_offset
+  LengthFactor_Offset=length(Factor_Offset)
+  
+  #### y scale_factor and y offset -----
+  y.scale_factor = Factor_Offset[LengthFactor_Offset-3] %>% 
+    str_extract(number_pattern) %>% 
+    as.numeric()
+  y.offset = Factor_Offset[LengthFactor_Offset-2] %>% 
+    str_extract(number_pattern) %>% 
+    as.numeric()
+  
+  #### x scale_factor and x offset -----
+  
+  x.scale_factor = Factor_Offset[LengthFactor_Offset-1] %>% 
+    str_extract(number_pattern) %>% 
+    as.numeric()
+  x.offset = Factor_Offset[LengthFactor_Offset] %>% 
+    str_extract(number_pattern) %>% 
+    as.numeric()
+  
+  #### semi_major_axis  ----
+  
+  r.eq <- print.output %>% 
+    str_subset("semi_major_axis") %>% 
+    str_extract(number_pattern) %>% 
+    as.numeric()
+  
+  #### semi_minor_axis  ----
+  
+  r.pol <- print.output %>% 
+    str_subset("semi_minor_axis") %>% 
+    str_extract(number_pattern) %>% 
+    as.numeric()
+  
+  #### 1st eccentricity -----
+  
+  e.value <- sqrt((r.eq^2-r.pol^2)/r.eq^2)
+  
+  
+  #### perspective point height -----
+  
+  PPH <- print.output %>% #perspective point height
+    str_subset("perspective_point_height") %>% 
+    str_extract(number_pattern) %>% 
+    as.numeric()
+  
+  #### Satellite height from center of earth (m) -----
+  
+  H <-  PPH + r.eq
+  
+  
+  #### longitude_of_projection_origin (rad) -----
+  lambda.o <- print.output %>% 
+    str_subset("longitude_of_projection_origin") %>% 
+    str_extract(number_pattern) %>% 
+    as.numeric() %>% #It is in degrees
+    deg2rad() #converting to radians
+  
+  list(y.scale_factor = y.scale_factor, y.offset = y.offset,
+       x.scale_factor = x.scale_factor, x.offset = x.offset,
+       r.eq = r.eq,
+       r.pol = r.pol,
+       e.value = e.value,
+       PPH = PPH,
+       H = H,
+       lambda.o = lambda.o)
+}
+
+filename = "Data/OR_ABI-L1b-RadC-M3C02_G16_s20172330202189_e20172330204562_c20172330205000.nc"
+NC_file <- nc_open(filename)
+
+NC_info <- File_info(NC_file)
+
+Coord.to.index <- function(lat,long, NC_infolist){
+  #Coords have to be in GRS80
+  
+  RawCoord<- coords.to.YX(lat,long, NC_infolist)
+  
+  y.index <- RawCoord$y.rad %>% 
+    multiply_by(NC_infolist$y.scale_factor) %>% 
+    add(NC_infolist$y.offset) %>% 
+    round()
+  
+  x.index <- RawCoord$x.rad %>% 
+    multiply_by(NC_infolist$x.scale_factor) %>% 
+    add(NC_infolist$x.offset) %>% 
+    round()
+  
+  list(y.index = y.index, x.index = x.index)
+}
+
+testlat = deg2rad(29.425171)
+testlong = deg2rad(-98.494614)
+
+coords.to.YX(0.590726966, -1.47813561, NC_info)
+Coord.to.index(0.590726966, -1.47813561, NC_info)
+
