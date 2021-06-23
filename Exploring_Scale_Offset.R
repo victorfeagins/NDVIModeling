560237451efb328a39f8d3e7f1c2a8c432773024
victorfeagins <victorfeagins@att.net>
2021-06-23 09:58:34 -0400

    Added Kappa to calculations, Added plots

--- Exploring_Scale_Offset.R
+++ Exploring_Scale_Offset.R
@@ -15,16 +15,20 @@ NDVICal <- function(R2,R3){
 }
 
 
+
 df.small <-  df.small %>% 
   mutate(R2 = RadC02 * KappaC02,
          R3 = RadC03 * KappaC03,
          NDVI = NDVICal(R2,R3),
-         R2_scaled = (RadC02 - RadC02Offset)/RadC02ScaleFactor,
-         R3_scaled = (RadC03 - RadC03Offset)/RadC03ScaleFactor,
+         R2_scaled = ((RadC02 - RadC02Offset)/RadC02ScaleFactor)*KappaC02,
+         R3_scaled = ((RadC03 - RadC03Offset)/RadC03ScaleFactor)*KappaC03,
          NDVI_scaled = NDVICal(R2_scaled,R3_scaled),
-         R2_scaled_Test = (RadC02 * RadC02ScaleFactor) + RadC02Offset,
-         R3_scaled_Test = (RadC03 * RadC03ScaleFactor) + RadC03Offset,
+         R2_scaled_Test = ((RadC02 * RadC02ScaleFactor) + RadC02Offset)* KappaC02,
+         R3_scaled_Test = ((RadC03 * RadC03ScaleFactor) + RadC03Offset)* KappaC03,
          NDVI_scaled_Test =  NDVICal(R2_scaled_Test,R3_scaled_Test))
+
+
+
 
 
 suntimes<- df.small %$% 
@@ -37,14 +41,20 @@ suntimes<- df.small %$%
 Daytime.small <-  df.small %>% 
   filter(Time > suntimes$nauticalDawn, Time < suntimes$nauticalDusk)
 
+
+# Comparing variables with itself
 Daytime.small %>% 
   select(Time, starts_with("R2"))%>% 
   reshape2::melt("Time") %>%
   mutate(Time = as.POSIXct(Time)) %>% 
   ggplot(mapping = aes(x = Time , y = value)) +
   geom_line(aes(group = variable, color = variable)) +
-  labs(title = "Values Scaled", x = "Time")+
+  facet_wrap(~variable, scales = "free")+
+  labs(title = "R2", x = "Time") +
   scale_x_datetime(labels = date_format("%H:%M:%S"))
+ 
+
+
 
 Daytime.small %>% 
   select(Time, starts_with("R3"))%>% 
@@ -52,8 +62,10 @@ Daytime.small %>%
   mutate(Time = as.POSIXct(Time)) %>% 
   ggplot(mapping = aes(x = Time , y = value)) +
   geom_line(aes(group = variable, color = variable)) +
-  labs(title = "Values Scaled", x = "Time")+
+  facet_wrap(~variable, scales = "free")+
+  labs(title = "R3", x = "Time") +
   scale_x_datetime(labels = date_format("%H:%M:%S"))
+
 
 Daytime.small %>% 
   select(Time, starts_with("NDVI"))%>% 
@@ -61,7 +73,44 @@ Daytime.small %>%
   mutate(Time = as.POSIXct(Time)) %>% 
   ggplot(mapping = aes(x = Time , y = value)) +
   geom_line(aes(group = variable, color = variable)) +
-  labs(title = "Values Scaled", x = "Time")+
+  facet_wrap(~variable, scales = "free")+
+  labs(title = "NDVI", x = "Time") +
   scale_x_datetime(labels = date_format("%H:%M:%S"))
+
+
+#Comparing variables with different variables
+Daytime.small %>% 
+  select(Time, R2,R3,NDVI)%>% 
+  reshape2::melt("Time") %>%
+  mutate(Time = as.POSIXct(Time)) %>% 
+  ggplot(mapping = aes(x = Time , y = value)) +
+  geom_line(aes(group = variable, color = variable)) +
+  #facet_wrap(~variable, scales = "free")+
+  labs(title = "TrueValue = data", x = "Time") +
+  scale_x_datetime(labels = date_format("%H:%M:%S"))
+
+
+Daytime.small %>% 
+  select(Time, ends_with("scaled"))%>% 
+  reshape2::melt("Time") %>%
+  mutate(Time = as.POSIXct(Time)) %>% 
+  ggplot(mapping = aes(x = Time , y = value)) +
+  geom_line(aes(group = variable, color = variable)) +
+  #facet_wrap(~variable, scales = "free")+
+  labs(title = "Truevalue = (data-offset)/scalefactor", x = "Time") +
+  scale_x_datetime(labels = date_format("%H:%M:%S"))
+
+
+Daytime.small %>% 
+  select(Time, ends_with("test"))%>% 
+  reshape2::melt("Time") %>%
+  mutate(Time = as.POSIXct(Time)) %>% 
+  ggplot(mapping = aes(x = Time , y = value)) +
+  geom_line(aes(group = variable, color = variable)) +
+  facet_wrap(~variable, scales = "free")+
+  labs(title = "Truevalue = data*scalefactor + offset", x = "Time") +
+  scale_x_datetime(labels = date_format("%H:%M:%S"))
+
+
 
 
