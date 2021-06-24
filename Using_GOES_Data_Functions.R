feddaab3a735a1c3330f1f4ddb1d7606361ad9fb
victorfeagins <victorfeagins@att.net>
2021-06-24 16:47:26 -0400

    Testing it out

--- Using_GOES_Data_Functions.R
+++ Using_GOES_Data_Functions.R
@@ -2,13 +2,16 @@ source("GOES_Data_Functions.R")
 
 
 #Input ----
-Datadirectory = "/projectnb/dietzelab/GOES_DataFTP/"
+Datadirectory = "Data"
 Latiude = 32.457 #Can be a vector, in degrees 
 Longitude = -91.9743 #Can be a vector, in degrees
-numcores = 9 #For sequential put 1
+numcores = 4 #For sequential put 1
 
 
 
 plan(multisession, workers = numcores)
 
 df = Extract_Dataframe_P(Datadirectory, Latiude, Longitude)
+
+
+
