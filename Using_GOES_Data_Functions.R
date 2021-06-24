409860059bb0306d53ae392ac6f31804e75d53d9
victorfeagins <victorfeagins@att.net>
2021-06-24 17:37:23 -0400

    Functions working well

--- Using_GOES_Data_Functions.R
+++ Using_GOES_Data_Functions.R
@@ -11,7 +11,26 @@ numcores = 4 #For sequential put 1
 
 plan(multisession, workers = numcores)
 
-df = Extract_Dataframe_P(Datadirectory, Latiude, Longitude)
+df = Extract_Dataframe_P(Datadirectory, Latiude, Longitude, average = TRUE)
 
 
+
+# file = "Data/OR_ABI-L1b-RadC-M3C02_G16_s20172330232189_e20172330234562_c20172330234599.nc"
+# Open_Extract_Value(file, Latiude, Longitude, TRUE)
+# 
+# file = "Data/OR_ABI-L1b-RadC-M4C03_G16_s20172330140227_e20172330145029_c20172330145056.nc"
+# Open_Extract_Value(file, Latiude, Longitude, TRUE)
+# 
+# 
+# file = "Data/OR_ABI-L2-ACMC-M4_G16_s20172330150227_e20172330150227_c20172330156258.nc"
+# Open_Extract_Value(file, Latiude, Longitude, TRUE)
+
+# 
+# NC_file <- nc_open(file)
+# NC_info <- File_info(NC_file)
+# FileRow<- Extract_Variable(Latiude,Longitude,NC_file,NC_info, TRUE) %>% 
+#   data.frame()
+# nc_close(NC_file)
+
+write.csv(df, "TestDataAvg.csv")
 
