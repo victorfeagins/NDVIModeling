c67f66f3f92b7969046d2e6682314097a2470582
victorfeagins <victorfeagins@att.net>
2021-06-24 17:37:09 -0400

    Avg has been implented. "BCM" is the variable name in ncfiles and that is what we will stick with.

--- GOES_Data_Functions.R
+++ GOES_Data_Functions.R
@@ -195,8 +195,8 @@ Extract_Variable <- function(lat, long, NC_file, NC_infolist, average = FALSE){
   filename <-  NC_file$filename 
   
   if(str_detect(filename, "OR_ABI-L2-ACMC")){
-    Varname <-  "ACM"
-    Outputname <-  "ACM"
+    Varname <-  "BCM"
+    Outputname <-  "BCM"
   } else if (str_detect(filename, "L1b-RadC-M[\\d]C02_G16")){
     Varname <- "Rad"
     Outputname <- "RadC02"
@@ -227,9 +227,14 @@ Extract_Variable <- function(lat, long, NC_file, NC_infolist, average = FALSE){
       Offset <- NC_file$var[[Varname]]$addOffset
       ScaleFact <- NC_file$var[[Varname]]$scaleFact 
     }
-  }} else if(average == TRUE){
-    y.window =  index$y.index[i]-1 :  index$y.index[i]+1
-    x.window = index$x.index[i]-1 :  index$x.index[i]+1
+  }}
+  
+  if(average == TRUE){
+    for (i in 1:length(lat)){
+    y.window =  (index$y.index[i]-1):(index$y.index[i]+1)
+    x.window = (index$x.index[i]-1):(index$x.index[i]+1)
+    
+    
     if (NC_file$var[[Varname]]$hasScaleFact){
       Value[i] <- mean(nc.get.var.subset.by.axes(NC_file, Varname, list(Y=y.window, X=x.window)))
     } else {
@@ -238,6 +243,7 @@ Extract_Variable <- function(lat, long, NC_file, NC_infolist, average = FALSE){
     }
     
     DataFlag[i] <- mean(nc.get.var.subset.by.axes(NC_file, "DQF", list(Y=y.window, X=x.window)))
+    }
     if (Varname == "Rad"){
       Kappa <-  ncvar_get(NC_file,"kappa0")
       Offset <- NC_file$var[[Varname]]$addOffset
@@ -274,22 +280,22 @@ Extract_Variable <- function(lat, long, NC_file, NC_infolist, average = FALSE){
                 RadC03Offset = Offset) %>% 
              data.frame())
     
-  } else if(Outputname == "ACM"){
+  } else if(Outputname == "BCM"){
     return(list(Latitude = Lat, Longitude = Long, 
-                ACM = Value,
+                BCM = Value,
                 Time = Time,
-                ACMDQF = DataFlag)) %>% 
+                BCMDQF = DataFlag)) %>% 
       data.frame()}
 }
 
 
-Open_Extract_Value <- function(file, lat, long){
+Open_Extract_Value <- function(file, lat, long, average = FALSE){
   #Opens and closes the files after getting info
   #Lat and Long can be vectors
   #Returns dataframe of row(s) of data per lat and long
   NC_file <- nc_open(file)
   NC_info <- File_info(NC_file)
-  FileRow<- Extract_Variable(lat,long,NC_file,NC_info) %>% 
+  FileRow<- Extract_Variable(lat,long,NC_file,NC_info, average) %>% 
     data.frame()
   nc_close(NC_file)
   return(FileRow)
@@ -297,7 +303,7 @@ Open_Extract_Value <- function(file, lat, long){
 
 
 
-Extract_Dataframe_P <- function(DataDirectory, lat, long){
+Extract_Dataframe_P <- function(DataDirectory, lat, long, average = FALSE){
   #Eventually put in Time day as variable to filter files day
   #Uses all the functions to create large dataframe containing info on all the 
   #Files in the datadirectory
@@ -313,11 +319,11 @@ Extract_Dataframe_P <- function(DataDirectory, lat, long){
   CloudMask <-  str_subset(files, "OR_ABI-L2-ACMC")
   
   
-  DataCh2<- future_map_dfr(Channel2files, Open_Extract_Value, lat = lat, long = long)
+  DataCh2<- future_map_dfr(Channel2files, Open_Extract_Value, lat = lat, long = long, average = average)
   
-  DataCh3<- future_map_dfr(Channel3files, Open_Extract_Value, lat = lat, long = long)
+  DataCh3<- future_map_dfr(Channel3files, Open_Extract_Value, lat = lat, long = long, average = average)
   
-  DataCloud<- future_map_dfr(CloudMask, Open_Extract_Value, lat = lat, long = long)
+  DataCloud<- future_map_dfr(CloudMask, Open_Extract_Value, lat = lat, long = long, average = average)
   
   FinalData <- merge(DataCh2,DataCh3, by = c("Time", "Latitude", "Longitude"), all = TRUE) %>%
     merge(DataCloud, by = c("Time", "Latitude", "Longitude"),all = TRUE)
