1b2a4c8b18e02f02bb1cafb43dc75f1adf3aa9b9
victorfeagins <victorfeagins@att.net>
2021-06-25 09:33:42 -0400

    The indexing is the same for all the functions

--- Comparing_NDVI_And_GOESFunctions.R
+++ Comparing_NDVI_And_GOESFunctions.R
@@ -5,6 +5,9 @@ library(GOESDiurnalNDVI)
 Latiude = 32.457 
 Longitude = -91.9743 
 
+
+
+# Channel 2 ----
 file = "Data/OR_ABI-L1b-RadC-M3C02_G16_s20172330202189_e20172330204562_c20172330205000.nc"
 
 
@@ -14,7 +17,7 @@ NC_info <- File_info(NC_File)
 
 coords.to.angle(Latiude, Longitude, NC_info)
 
-coords.to.index(Latiude, Longitude, NC_info)
+Ch2.index<- coords.to.index(Latiude, Longitude, NC_info)
 
 
 Lat.rad = deg2rad(Latiude)
@@ -23,5 +26,49 @@ Long.rad = deg2rad(Longitude)
 
 getABI_Index(Lat.rad, Long.rad, "OLD") %>% 
   getDataIndex(2, "OLD")
+
+
+
+
+
+
+nc_close(NC_File)
+
+# Channel 3 -----
+
+file = "Data/OR_ABI-L1b-RadC-M4C03_G16_s20172330030227_e20172330035029_c20172330035058.nc"
+NC_File <- nc_open(file)
+NC_info <- File_info(NC_File)
+
+coords.to.angle(Latiude, Longitude, NC_info)
+
+coords.to.index(Latiude, Longitude, NC_info)
+
+
+getABI_Index(Lat.rad, Long.rad, "OLD") %>% 
+  getDataIndex(3, "OLD")
+
+
+nc_close(NC_File)
+
+
+# Cloud ------
+
+file = "Data/OR_ABI-L2-ACMC-M4_G16_s20172330145227_e20172330145227_c20172330151279.nc"
+NC_File <- nc_open(file)
+NC_info <- File_info(NC_File)
+
+coords.to.angle(Latiude, Longitude, NC_info)
+
+coords.to.index(Latiude, Longitude, NC_info)
+
+
+getABI_Index(Lat.rad, Long.rad, "OLD") %>% 
+  getDataIndex("ACM", "OLD")
+
+
+nc_close(NC_File)
+
+
 
 
