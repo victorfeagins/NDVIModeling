ce0e1e93e6fb50488883d4b7897bdcce24d148d7
victorfeagins <victorfeagins@att.net>
2021-06-29 14:42:12 -0400

    future_lapply worked

--- MultiSite_Modeling.R
+++ MultiSite_Modeling.R
@@ -1,12 +1,22 @@
-library(suncalc)
-library(dplyr)
-library(magrittr)
-library(ggplot2)
-library(lubridate) 
-library(scales)
+#Input ----
 
-library(GOESDiurnalNDVI)
+csvfile = "/projectnb/dietzelab/vfeagins/Programming/NVDI_Modeling/MultiSiteJan_01_07.csv"
+numCores = 9
 
+#Packages ----
+library(suncalc) #Used to find suntimes
+library(dplyr) #Used to manuiplate dataframes
+library(magrittr) #Used for pipes
+library(ggplot2) #Used for graphing
+library(lubridate)# Uses to create intervals
+library(scales) #Used to create nice axis
+library(GOESDiurnalNDVI) #Used for running NDVI modeling and calculating NDVI
+
+library(future)
+library(future.apply)
+
+
+#Functions ----
 GroupIDs <-  function(dataframe){
   #Creates ID Variables for day of year and position
   output <- dataframe %>% 
@@ -47,25 +57,6 @@ NDVIQuality <- function(dataframewithNDVI){
   return(output)
 }
 
-df <- read.csv("MultiSiteJan_01_07.csv")
-
-df.clean <- df %>% 
-  GroupIDs() %>% 
-  NDVICreate() %>% 
-  NDVIQuality() #Quality needs group variables for Daytime
-
-
-
-df.model.vectors <- df.clean %>% 
-  select(Time, NDVI, DaySiteID) %>% 
-  mutate(Time = hour(Time) + minute(Time)/60) %>% 
-  rename(x = Time, y = NDVI) %>% 
-  group_by(DaySiteID) %>% 
-  filter(n() >= 50)%>% 
-  group_split() %>% 
-  lapply(as.list)
-
- 
 DiurnalModeling <- function(Data){
   j.model = createDiurnalModel("Test", Data)
   var.burn <- runMCMC_Model(j.model=j.model,variableNames=c("a","c","k","prec"),
@@ -75,15 +66,52 @@ DiurnalModeling <- function(Data){
   return(var.burn)
 }
 
-library(parallel)
+
+# Reading in data ----
+df <- read.csv(csvfile)
 
 
+# Cleaning data ----
+df.clean <- df %>% 
+  GroupIDs() %>% 
+  NDVICreate() %>% 
+  NDVIQuality() #Quality needs group variables for applying Daytime
+
+
+# Preparing data for modeling ----
+df.model.vectors <- df.clean %>% 
+  select(Time, NDVI, DaySiteID) %>% 
+  mutate(Time = hour(Time) + minute(Time)/60) %>% #Eventually might need to convert to local time zone
+  rename(x = Time, y = NDVI) %>% 
+  group_by(DaySiteID) %>% 
+  filter(n() >= 25)%>% #Keep onlysite day that have more then 25 obs
+  group_split() %>% #Splits groups up into a list
+  lapply(as.list) #Diurnal modeling wants lists
+
+ 
+
+
+
+
+
+# library(parallel)
+# 
+# 
+# ptm <- proc.time()
+# 
+# modeloutput = mclapply(df.model.vectors, DiurnalModeling, mc.cores = 9)
+# (Time<- proc.time() - ptm) #2597.945 seconds
+
+
+test <- df.model.vectors[1:numCores] #I don't want to wait 2 hours or more so just testing
+
+plan(multisession, workers = numCores)
 ptm <- proc.time()
+future_lapply(test, DiurnalModeling)
 
-modeloutput = mclapply(df.model.vectors, DiurnalModeling, mc.cores = 9)
+
 (Time<- proc.time() - ptm) #2597.945 seconds
-
-
+#save(modeloutput, file ="TestModel6sites_days")
 
 
 
