e012e4ba88edf9ff683b894582f577d183369dbd
victorfeagins <victorfeagins@att.net>
2021-07-01 14:13:28 -0400

    Started work on Creating pipeline for forecasting

---
+++ Extract_Data_Pipeline.R
@@ -0,0 +1,24 @@
+source("GOES_Data_Functions.R")
+Datadirectory = "/projectnb/dietzelab/GOES_DataFTP/GOES_Data_2020/"
+numcores = 9 
+SiteCodeBook = "SiteCodeBook.csv"
+Today = Sys.Date()
+Daysback = 1
+
+SiteCodedf = read.csv(SiteCodeBook)
+
+Latitude = SiteCodedf$Latitude
+Longitude = SiteCodedf$Longitude
+
+Dates <- seq(Today - Daysback, Today-1, by="days") 
+
+
+
+ptm <- proc.time()
+plan(multisession, workers = numcores)
+
+df = Extract_Dataframe_P(Datadirectory, Latitude, Longitude, Dates, average = TRUE)
+
+(Time<- proc.time() - ptm)
+
+
