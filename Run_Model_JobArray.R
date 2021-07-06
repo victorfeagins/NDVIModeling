fe4b9daa0321b24b12f875fd669ea6e669142b5b
victorfeagins <victorfeagins@att.net>
2021-07-06 14:21:36 -0400

    Next step is to figure the best way to save the mcmc list

--- Run_Model_JobArray.R
+++ Run_Model_JobArray.R
@@ -1,16 +1,51 @@
+#Inputs ----
 inputdirectory = "/projectnb/dietzelab/GOES_DataFTP/InputFilesNDVIModel/2021/"
-
 outputdirectory = "/projectnb/dietzelab/GOES_DataFTP/OutputFilesNDVIModel/2021/"
 
+
+fileindex = as.numeric(commandArgs(TRUE)[1]) #Comes from job array
+
+
+#Packages ----
 library(lubridate)
 library(GOESDiurnalNDVI) #Used for running NDVI modeling and calculating NDVI
 library(stringr)
 
+# Functions ----
+DiurnalModeling <- function(Data){
+  ModelVectors = list(x = Data$x, y = Data$y)
+  j.model = createDiurnalModel("Test", ModelVectors)
+  var.burn <- runMCMC_Model(j.model=j.model,variableNames=c("a","c","k","prec"),
+                              baseNum=20000,iterSize =10000)
+  out.mat <- as.matrix(var.burn)
+  thinAmount <- round(nrow(out.mat)/5000,digits=0)
+  var.burn <- window(var.burn,thin=thinAmount)
+  attr(var.burn, "DaySiteID") <- unique(Data$DaySiteID)
+  return(var.burn) 
+}
 
+#Extracting single file ----
 Today = Sys.Date()
 Daysback = 1
 
 Dates <- seq(Today - Daysback, Today-1, by="days")
 
-inputfiles = list.files(inputdirectory)
+inputfiles = list.files(inputdirectory) %>% 
+  str_subset(str_c(year(Dates),yday(Dates), sep = "_"))
+
+
+file = inputfiles[1]
+
+Data = file.path(inputdirectory, file) %>% 
+  read.csv() %>% 
+  as.list()
+
+
+#Modeling Single file ----
+ptm <- proc.time()
+
+
+OutPutModel <- DiurnalModeling(Data)
+
+print(proc.time() - ptm)
 
