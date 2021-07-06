3491e57fe078de3df0fbd1f3d4f1c1d843de8ad7
victorfeagins <victorfeagins@att.net>
2021-07-06 10:16:01 -0400

    sh files for extracting data from ncfiles

---
+++ ExtractDataNCFiles.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+
+#$ -N ExtractDataNCFile #Name of job
+#$ -pe omp 4 #Grab 4 cores
+
+# Set SCC project
+#$ -P dietzelab
+
+module load R/4.0.5
+
+Rscript /projectnb/dietzelab/vfeagins/Programming/NVDI_Modeling/Extract_Data_Cron.R $NSLOTS
